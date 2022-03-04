90a90e7042df8410a2d43d3dd36db68ccdbf2992
ci3612px <ci3612px@go.minnstate.edu>
2022-03-04 13:50:45 -0600

    Made Login Layout pretty

--- app/src/main/java/com/csis365/assignment1/LoginActivity.kt
+++ app/src/main/java/com/csis365/assignment1/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.csis365.assignment1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.SharedPreferences
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
