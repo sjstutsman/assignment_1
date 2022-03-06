ae2968a03f168fdaaee1cb0f93c7bb1401462dea
elkana-m <munganga.elkana@yahoo.fr>
2022-03-06 11:29:34 -0600

    Added FruitActivity

---
+++ app/src/main/java/com/csis365/assignment1/FruitActivity.java
@@ -0,0 +1,4 @@
+package com.csis365.assignment1;
+
+public class FruitActivity {
+}
---
+++ app/src/main/java/com/csis365/assignment1/LoginActivity.kt
@@ -0,0 +1,34 @@
+package com.csis365.assignment1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var etUsername: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var btnLogin: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        etUsername = findViewById(R.id.et_username)
+        etPassword = findViewById(R.id.et_password)
+        btnLogin = findViewById(R.id.btn_login)
+
+        val actionBar = supportActionBar
+        actionBar!!.title = "Log In"
+
+        btnLogin.setOnClickListener {
+
+            val UserName = etUsername
+            val Password = etPassword
+
+            val intent = Intent(this, fruitActivaty::class.java)
+        }
+    }
+}
