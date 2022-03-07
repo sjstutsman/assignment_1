551d5169b4d504a74203a717c149527de1f1cc9a
ci3612px <ci3612px@go.minnstate.edu>
2022-03-07 13:49:39 -0600

    Shared Preferences example to make it easier to implement later

--- app/src/main/java/com/csis365/assignment1/LoginActivity.kt
+++ app/src/main/java/com/csis365/assignment1/LoginActivity.kt
@@ -1,12 +1,30 @@
 package com.csis365.assignment1
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.SharedPreferences
+import android.content.SharedPreferences.Editor
 
 class LoginActivity : AppCompatActivity() {
+
+    private val keyFirstName = "key_name"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
+
+    val prefs : SharedPreferences = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE)
+    with(prefs.edit())
+    {
+        putString(keyFirstName, "Andrew")
+        commit()
+    }
+
+    val prefs2 = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE)
+    val firstName = prefs2.getString(keyFirstName, "default value")
+
+    Log.d("tag", firstName.toString())
+
 }
