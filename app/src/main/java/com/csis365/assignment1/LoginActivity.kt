9f3e3817aacb0037b7672a079e1311ec774d5d9d
ci3612px <ci3612px@go.minnstate.edu>
2022-03-09 14:06:33 -0600

    Worked on Login Activity, checked if authorized

--- app/src/main/java/com/csis365/assignment1/LoginActivity.kt
+++ app/src/main/java/com/csis365/assignment1/LoginActivity.kt
@@ -1,28 +1,57 @@
 package com.csis365.assignment1
 
 import android.content.Context
-import android.util.Log
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
 
-    private val keyFirstName = "key_name"
+    private val keyAuthorization = "key_authorization"
+    private val keyPassword = "key_password"
+    private lateinit var loginSubmit : Button
+    private lateinit var loginPassword : EditText
+    private lateinit var loginUsername : EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val prefs : SharedPreferences = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE)
+        val editor  = prefs.edit()
+        if(prefs.getBoolean(keyAuthorization, false)) {
+
+        } else {
+            editor.putBoolean(keyAuthorization, false).commit()
+        }
+        editor.putString(keyPassword, "password").commit()
+
+
+        val prefs2 = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE)
+        val firstName = prefs2.getString(keyAuthorization, "default value")
+
+        loginSubmit = findViewById(R.id.login_submit)
+        loginUsername = findViewById(R.id.login_username)
+        loginPassword = findViewById(R.id.login_password)
+
+        loginSubmit.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                val prefs = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE)
+                val password = prefs2.getString(keyPassword, "default value")
+                if(loginPassword.text.toString() == password && loginUsername.text.isNotEmpty()){
+                    val editor = prefs.edit()
+                    editor.putBoolean(keyAuthorization, true)
+                }
+            }
+
+        })
+
+
+
+        Log.i("tag", firstName.toString())
     }
-
-    val prefs : SharedPreferences = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE)
-    val editor : SharedPreferences.Editor = prefs.edit()
-    editor.putString(keyFirstName, "Andrew").commit()
-
-
-    val prefs2 = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE)
-    val firstName = prefs2.getString(keyFirstName, "default value")
-
-    Log.i("tag", firstName)
-
 }
--- app/src/main/java/com/csis365/assignment1/MyItemAdapter.kt
+++ app/src/main/java/com/csis365/assignment1/MyItemAdapter.kt
@@ -3,28 +3,29 @@ package com.csis365.assignment1
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class MyItemAdapter(val data: List<String>) : RecyclerView.Adapter<MyItemAdapter.MyViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.view_simple_textview, parent, false)
+            .inflate(R.layout.view_simple_button, parent, false)
 
         return MyViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.textView.text = data[position]
+        holder.button.text = data[position]
     }
 
     override fun getItemCount() = data.size
 
     class MyViewHolder(view: View) :  RecyclerView.ViewHolder(view){
-        val textView: TextView
+        val button: Button
 
         init{
-            textView = view.findViewById(R.id.tv_data)
+            button = view.findViewById(R.id.bt_data)
         }
     }
 
