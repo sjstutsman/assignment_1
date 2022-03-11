de745b4cf02f14938d96c779c24fcb178e02b2b8
JaredRLanoue <91082238+JaredRLanoue@users.noreply.github.com>
2022-03-10 22:45:31 -0600

    Created validateLogin function and added a textview error message for failed password attempts.

--- app/src/main/java/com/csis365/assignment1/List.kt
+++ app/src/main/java/com/csis365/assignment1/List.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class List : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_adapter)
+        setContentView(R.layout.activity_list)
         title ="Fruit List"
     }
 }
--- app/src/main/java/com/csis365/assignment1/Login.kt
+++ app/src/main/java/com/csis365/assignment1/Login.kt
@@ -3,27 +3,41 @@ package com.csis365.assignment1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import org.w3c.dom.Text
 
 class Login : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         title = "Login Page"
 
         val loginButton = findViewById<Button>(R.id.login_btn)
         val username = findViewById<EditText>(R.id.username)
         val password = findViewById<EditText>(R.id.password)
 
-        loginButton.setOnClickListener {
-            if(username.text.length >=(4) && password.text.toString() == "password") {
-                startActivity(Intent(this, List::class.java))
+
+        fun validateLogin(username: EditText, password: EditText) {
+            loginButton.setOnClickListener {
+                if (username.text.length >= (4) && password.text.toString() == "password") {
+                    startActivity(Intent(this, List::class.java))
+                }
+                else {
+                    val loginError = findViewById<TextView>(R.id.error_message)
+                    loginError.visibility = View.VISIBLE
+                }
             }
-            else
-                Toast.makeText(applicationContext, "Login failed, please try again!", Toast.LENGTH_SHORT).show()
         }
+
+
+        validateLogin(username, password)
+
+
     }
+
 }
