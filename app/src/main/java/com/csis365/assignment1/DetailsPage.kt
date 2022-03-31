9e3659a72f3bf50fad48d52bba6770fbaf8460f2
abenezermario <abenezerjava@gmail.com>
2022-03-31 18:30:02 -0500

    modified details page

--- app/src/main/java/com/csis365/assignment1/DetailsPage.kt
+++ app/src/main/java/com/csis365/assignment1/DetailsPage.kt
@@ -11,18 +11,20 @@ class DetailsPage : AppCompatActivity() {
 
     private lateinit var details: TextView
     private lateinit var nutrition_detail: TextView
+    private lateinit var genus: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details_page)
 
         details = findViewById<TextView>(R.id.details)
         nutrition_detail = findViewById<TextView>(R.id.nutrition_detail)
+        genus = findViewById<TextView>(R.id.genus)
 
         var fruit = intent.extras?.get("fruit") as Fruit
 
         var list = arrayOf(fruit)
         for (item in list) {
-            details.append("genus: " + item.genus + "\n" + "family: " + item.family + "\n" + "order: " + item.order + "\n")
+            details.append("Genus: " + item.genus + "\n" + "family: " + item.family + "\n" + "order: " + item.order + "\n")
             nutrition_detail.append(
                 "Carbohydrate: " + item.nutritions.carbohydrates + "\n" + "Protein: " + item.nutritions.protein + "\n"
                         + "Calories: " + item.nutritions.calories + "\n" + "Fat: " + item.nutritions.fat + "\n" + "Sugar: " + item.nutritions.sugar + "\n"
